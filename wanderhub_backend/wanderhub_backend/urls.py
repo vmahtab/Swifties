fccdca9e7cc3b0d783c03b805951db8859de2702
Hasan Berk Zengin <zenginhasanberk@gmail.com>
2024-03-22 23:33:08 -0400

    Finish authentication with default User model

--- wanderhub_backend/wanderhub_backend/urls.py
+++ wanderhub_backend/wanderhub_backend/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from api import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('login', views.login),
+    path('signup', views.signup),
+    path('test_token', views.test_token),
 ]
