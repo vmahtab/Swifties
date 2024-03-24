f56d2308e3e3b1c998d7d13c756d1e5594c6e4be
Hasan Berk Zengin <zenginhasanberk@gmail.com>
2024-03-24 00:07:22 -0400

    Finish work for skeletal product

--- wanderhub_backend/api/models.py
+++ wanderhub_backend/api/models.py
@@ -1,4 +1,23 @@
 from django.db import models
-
+from django.conf import settings
 
 # Create your models here.
+class Landmark(models.Model):
+    id = models.AutoField(primary_key=True) 
+    name = models.CharField(max_length=200)
+    city_name = models.CharField(max_length=100)
+    country_name = models.CharField(max_length=100)
+    image_url = models.URLField(max_length=200)
+    
+
+    def __str__(self):
+        return self.name
+
+class VisitedCities(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    landmark = models.ForeignKey(Landmark, on_delete=models.CASCADE)
+    visit_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.user.username} visited {self.landmark.city_name} on {self.visit_time}"
