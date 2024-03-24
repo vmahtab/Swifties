ef298da5dcde825d59b3ab517a2a88e5aa2b8df7
Alexey Kovalenko <2000ako@gmail.com>
2024-03-24 04:23:43 -0400

    marging with authorization

--- wanderhub/wanderhub/SignIn/SignUp.swift
+++ wanderhub/wanderhub/SignIn/SignUp.swift
@@ -9,10 +9,34 @@ import SwiftUI
 
 struct SignUpView: View {
     @Environment(\.presentationMode) var presentationMode
-    @State private var username: String = "Traveller"
-    @State private var password: String = "funtravel"
+    @State private var username: String?
+    @State private var password: String?
+    @State private var email: String?
     var user = User.shared
     @Binding var signinProcess: Bool
+    
+    @State private var showAlert = false
+    @State private var loginFailed = false
+    
+    func signup() {
+        guard let username = username, !username.isEmpty,
+              let password = password, !password.isEmpty,
+              let email = email, !email.isEmpty else {
+            showAlert = true
+            return
+        }
+        Task {
+            do {
+                if let _ = await user.signup(username: username, password: password, email: email) {
+                    signinProcess.toggle()
+                    presentationMode.wrappedValue.dismiss()
+                } else {
+                    loginFailed = true
+                }
+            }
+        }
+        
+    }
     
     var body: some View {
         VStack {
@@ -22,7 +46,10 @@ struct SignUpView: View {
                 .fontWeight(.semibold)
                 .foregroundColor(titleCol)
             
-            TextField("Enter name", text: $username)
+            TextField("Enter name", text: Binding(
+                get: { username ?? "" },
+                set: { username = $0.isEmpty ? nil : $0 }
+            ))
                 .foregroundColor(greyCol)
                 .padding()
                 .background(
@@ -32,7 +59,21 @@ struct SignUpView: View {
                 )
                 .padding(.horizontal, 40)
             
-            SecureField("Enter password", text: $password)
+            SecureField("Enter password", text: Binding(get: {password ?? ""},
+                                                        set: {password = $0.isEmpty ? nil : $0}))
+                .foregroundColor(greyCol)
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(lineWidth: 1)
+                        .foregroundColor(greyCol)
+                )
+                .padding(.horizontal, 40)
+            
+            TextField("Enter Email", text: Binding(
+                get: { email ?? "" },
+                set: { email = $0.isEmpty ? nil : $0 }
+            ))
                 .foregroundColor(greyCol)
                 .padding()
                 .background(
@@ -43,12 +84,9 @@ struct SignUpView: View {
                 .padding(.horizontal, 40)
             
             Button("Sign Up") {
-                // TODO: PUT THIS BACK INSIDE ONCE CONNECTED WITH BACKEND
-                Task {
-                    await user.signup(username: username, password: password)
-                }
-                signinProcess.toggle()
-                presentationMode.wrappedValue.dismiss()
+                signup()
+//                signinProcess.toggle()
+//                presentationMode.wrappedValue.dismiss()
             }
             .padding()
             .frame(maxWidth: .infinity)
@@ -56,6 +94,12 @@ struct SignUpView: View {
             .foregroundColor(backCol)
             .cornerRadius(10)
             .padding(.horizontal, 40)
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("Error"), message: Text("Username and password cannot be empty"), dismissButton: .default(Text("OK")))
+            }
+            .alert(isPresented: $loginFailed) {
+                Alert(title: Text("Error"), message: Text("Login Failed"), dismissButton: .default(Text("OK")))
+            }
             
             Spacer()
         }
