a8bf10e5d50edd8a8d1780310976e84256ffde45
Ubuntu <ubuntu@wanderhub-backend.us-east1-b.c.wanderhub-418303.internal>
2024-04-09 15:50:36 +0000

    Add load_dotenv()

--- wanderhub_backend/api/landmarks.py
+++ wanderhub_backend/api/landmarks.py
@@ -31,6 +31,8 @@ import os
 
 from openai import OpenAI
 
+
+load_dotenv()
 api_key = os.getenv("API_KEY")
 client = OpenAI(api_key=api_key)
 
--- wanderhub_backend/api/views.py
+++ wanderhub_backend/api/views.py
@@ -23,9 +23,9 @@ import os
 
 import json
 
-load_dotenv()
-
 from openai import OpenAI
+
+load_dotenv()
 api_key = os.getenv("API_KEY")
 client = OpenAI(api_key=api_key)
 
